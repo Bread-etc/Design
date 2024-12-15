a7b9889ff7b7059fde8cc2844266f7943ed4c1fd
bread_etc <1054115624@qq.com>
2024-12-16 02:07:11 +0800

    add:HttpRequest module

---
+++ src/utils/http.ts
@@ -0,0 +1,43 @@
+import Request from "./request";
+import type { AxiosResponse } from "axios";
+import type { RequestConfig } from "./request/types";
+
+export interface HttpResponse<T> {
+	statusCode: number;
+	desc: string;
+	result: T;
+}
+
+// 重写返回类型
+interface HttpRequestConfig<T, R> extends RequestConfig<HttpResponse<R>> {
+	data?: T;
+}
+
+const request = new Request({
+	baseURL: import.meta.env.BASE_URL,
+	timeout: 1000 * 60 * 5,
+	interceptors: {
+		// 请求拦截器
+		requestInterceptors: (config) => config,
+		// 响应拦截器
+		responseInterceptors: (result: AxiosResponse) => {
+			return result;
+		},
+	},
+});
+
+/**
+ * @generic D 请求参数
+ * @generic T 响应结构
+ * @param {HttpRequestConfig} config 不管是GET还是POST请求都使用data
+ * @returns {Promise}
+ */
+const httpRequest = <D = any, T = any>(config: HttpRequestConfig<D, T>) => {
+	const { method = "GET" } = config;
+	if (method === "get" || method === "GET") {
+		config.params = config.data;
+	}
+	return request.request<HttpResponse<T>>(config);
+};
+
+export default httpRequest;
